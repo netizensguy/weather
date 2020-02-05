7597e4ff64274aa72d3f4087ce9681506aa09152
guquanjin <1052879268@qq.com>
2020-02-05 15:57:04 +0800

    init

---
+++ src/Weather.php
@@ -0,0 +1,46 @@
+<?php
+namespace Netizensguy\Weather;
+
+use GuzzleHttp\Client;
+
+class Weather
+{
+
+	protected $gaoDeKey;//高德地图的key,列如：3c4f5741a79827018790af984084a81d
+	 protected $guzzleOptions = []; 
+  
+    public function __construct($key)
+    {
+    	$this->gaoDeKey = $key;
+    }
+
+    public function getHttpClient()
+    {
+    	return new Client($this->guzzleOptions);
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+    	$this->guzzleOptions = $options;
+    } 
+
+    public function getWeather($city, $type = 'base', $format = 'json')
+    {
+        $url = "https://restapi.amap.com/v3/weather/weath";
+
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'output' => $format,
+            'extensions' =>  $type,
+        ]);
+
+        $response = $this->getHttpClient()->get($url, [
+            'query' => $query,
+        ])->getBody()->getContents()
+
+        return $format === 'json' ? json_decode($response) : $response;
+
+    }
+
+}
